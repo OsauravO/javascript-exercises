a976a5e2431ccb747fc1e3b7564a815a15842e58
OsauravO <saurav1hasyo@gmail.com>
2023-06-08 19:58:20 +0530

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,10 @@
-const repeatString = function(string,num) {
-   const ans="";
-   for(let i=0;i<num;i++){
-      ans+=string;
-   }
-    return ans;
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
 
-// Do not edit below this line
 module.exports = repeatString;
