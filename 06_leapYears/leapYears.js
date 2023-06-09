a58fa7ef7e87c4bd109fe2d591b225b491b078cf
OsauravO <saurav1hasyo@gmail.com>
2023-06-09 17:28:42 +0530

    Update leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+if(year%4===0){
+  if(year%100===0 && year%400===0){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+else{
+  return 0;
+}
+    
 };
 
 // Do not edit below this line
