c8b08875565f67a5b6b06678f5cc69459be1d048
OsauravO <saurav1hasyo@gmail.com>
2023-06-09 17:14:06 +0530

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+let i=start;
+  let sum=0;
+  for(i;i>=end;i++){
+    sum+=i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
