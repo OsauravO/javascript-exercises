d83b9f01c608b3e384aa1ddb36105cd159ee355d
OsauravO <saurav1hasyo@gmail.com>
2023-06-09 18:20:49 +0530

    Update fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+	if (count < 0) return "OOPS"
+	const fib = [0, 1];
+	for (let i = 1; i < count; i++) {
+		fib.push(fib[i] + fib[i -1]);
+	}
+	return fib[count];
 };
 
 // Do not edit below this line
