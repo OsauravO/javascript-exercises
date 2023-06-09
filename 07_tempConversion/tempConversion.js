3abfe15460e5db0a52c70e96c945da4029881ca5
OsauravO <saurav1hasyo@gmail.com>
2023-06-09 17:50:26 +0530

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,12 @@
-const convertToCelsius = function(temp) {
-  return Math.random((temp-32)*(5/9)*10)/10;
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function(temp) {
-  return Math.random(((temp*(9/5))+32)*10)/10;
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
-// Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
