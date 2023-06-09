0309e984f9f263bafb1df98283277fb7657c7015
OsauravO <saurav1hasyo@gmail.com>
2023-06-09 17:20:47 +0530

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
 const sumAll = function(start,end) {
+  
+if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+  if (start < 0 || end < 0) return "ERROR";
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
 let i=start;
   let sum=0;
   for(i;i<=end;i++){
